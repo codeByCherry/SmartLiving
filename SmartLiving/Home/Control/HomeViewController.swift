4b17bdf9a029d3c07c6e9798ea8dac61773cad25
codyByCherry <373466869@qq.com>
2018-10-25 15:17:37 +0800

    重构代码文件结构

---
+++ SmartLiving/Home/Control/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  SmartLiving
+//
+//  Created by Tony Zhang on 2018/10/25.
+//  Copyright © 2018年 SSM. All rights reserved.
+//
+
+import UIKit
+
+
+
+class HomeViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+    }
+
+}
---
+++ SmartLiving/Home/Model/DeviceModel.swift
@@ -0,0 +1,20 @@
+//
+//  DeviceModel.swift
+//  SmartLiving
+//
+//  Created by Tony Zhang on 2018/10/25.
+//  Copyright © 2018年 SSM. All rights reserved.
+//
+
+import Foundation
+
+
+class DeviceModel {
+    
+    var deviceImageName = ""
+    
+    var deviceName = ""
+    
+    var deviceDesc = ""
+    
+}
