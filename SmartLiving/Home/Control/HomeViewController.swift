878ea75f380d20c528d1c596c00de960816ce6e0
codyByCherry <373466869@qq.com>
2018-10-25 15:47:57 +0800

    构建设备列表的tableview

--- SmartLiving/Home/Control/HomeViewController.swift
+++ SmartLiving/Home/Control/HomeViewController.swift
@@ -10,13 +10,68 @@ import UIKit
 
 
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var deviceTableView: UITableView!
+    
+    var devices = [DeviceModel]()
+    
     
     override func viewDidLoad() {
 
         super.viewDidLoad()
-
+        
+        deviceTableView.rowHeight = 102
+        
+        let camera = DeviceModel()
+        camera.deviceName = "camera1"
+        camera.deviceImageName = "device_camera"
+        camera.deviceDesc = "这是一个个性十足的网络神奇"
+        
+        let camera2 = DeviceModel()
+        camera2.deviceName = "camera2"
+        camera2.deviceImageName = "device_camera"
+        camera2.deviceDesc = "这是一个个性十足的网络神奇"
+        
+        let camera3 = DeviceModel()
+        camera3.deviceName = "camera3"
+        camera3.deviceImageName = "device_camera"
+        camera3.deviceDesc = "这是一个个性十足的网络神奇"
+        
+        devices.append(camera)
+        devices.append(camera2)
+        devices.append(camera3)
+        
+        
     }
+    
+    
+    //MARK:- TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return devices.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let deviceCell = tableView.dequeueReusableCell(withIdentifier: "deviceCell", for: indexPath) as! DeviceCell
+        
+        let deviceModel = devices[indexPath.row]
+        
+        deviceCell.setup(with: deviceModel)
+        
+        return deviceCell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
 
 }
---
+++ SmartLiving/Home/View/DeviceCell.swift
@@ -0,0 +1,44 @@
+//
+//  DeviceCell.swift
+//  SmartLiving
+//
+//  Created by Tony Zhang on 2018/10/25.
+//  Copyright © 2018年 SSM. All rights reserved.
+//
+
+
+import UIKit
+
+
+
+class DeviceCell: UITableViewCell {
+
+
+    @IBOutlet weak var deviceImageView: UIImageView!
+    
+    
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var deviceDescLabel: UILabel!
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    
+    
+    func setup(with deviceModel:DeviceModel) {
+        
+        deviceImageView.image = UIImage(named: deviceModel.deviceImageName)
+        
+        deviceNameLabel.text = deviceModel.deviceName
+        
+        deviceDescLabel.text = deviceModel.deviceDesc
+        
+    }
+
+}
